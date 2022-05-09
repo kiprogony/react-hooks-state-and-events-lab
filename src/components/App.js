b5fe65154812fb0c6eb96efd42954a511e28bbc7
Daryl Rogony <darylrogony@gmail.com>
2022-05-09 18:19:19 +0300

    Completed Assiginment on updating state based on different events

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+// import { useState } from "react/cjs/react.production.min";
 
 function App() {
+  const[items, makeItems] = useState(itemData);
+  const[darkMode, toDarkMode] = useState(false);
+
+  function darkModeClick() {
+    toDarkMode((darkMode) => !darkMode);
+  }
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = darkMode ? "App dark" : "App light";
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={darkModeClick}>{darkMode ? "Dark" : "Light"} Mode </button>
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList items={items} />
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+// import { useState } from "react/cjs/react.production.min";
 
 function Item({ name, category }) {
+  const [insideCart, toInsideCat] = useState(false);
+
+  function addingToCart() {
+    toInsideCat((insideCart) => !insideCart);
+  }
+
   return (
-    <li className="">
+    <li className={insideCart ? "in-cart" : " "}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={insideCart ? " remove" : "add"}onClick={addingToCart}>{ insideCart ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
